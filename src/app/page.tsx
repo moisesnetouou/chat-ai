00ceba616a5d39fcd737c6652a741fda605e6dbd
Moisés <moisesnetored@gmail.com>
2023-07-18 11:51:43 -0400

    ai and vercel sdk ai added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Chat } from "@/components/Chat";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Chat />
+    </div>
+  );
 }
